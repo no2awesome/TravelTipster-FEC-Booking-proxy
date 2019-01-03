4a0868de3323495faa8bf1a3493492ccea1267d4
Shota Sugawara <shrikethrush@gmail.com>
2019-01-02 21:24:57 -0800

    fixed port

--- server/server.js
+++ server/server.js
@@ -3,7 +3,7 @@ const bodyParser = require('body-parser');
 const path = require('path');
 
 const app = express();
-const PORT = 3001;
+const PORT = 3000;
 
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({
