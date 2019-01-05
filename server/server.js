f33a04be85190daa202821a828748602d4eb12d6
Shota Sugawara <shrikethrush@gmail.com>
2019-01-04 20:22:14 -0800

    Added proxy functionality.

--- server/server.js
+++ server/server.js
@@ -1,6 +1,7 @@
 const express = require('express');
 const bodyParser = require('body-parser');
 const path = require('path');
+const axios = require('axios')
 
 const app = express();
 const PORT = 8081;
@@ -11,6 +12,23 @@ app.use(bodyParser.urlencoded({
 }));
 
 app.use(express.static('public'));
+app.get('/:hotel_id/', (req, res) => {
+    res.sendFile(path.join(__dirname, '../public', 'index.html'))
+});
+
+app.get('/:hotel_id/vacancy', (req, res) => {
+    //TODO - your code here!
+    axios.get(`http://traveltipster-fec-booking-service-dev.us-west-2.elasticbeanstalk.com/${req.originalUrl}`)
+        .then(function (response) {
+            console.log(response);
+            res.send(response);
+        })
+        .catch(function (error) {
+            console.log(error);
+        });
+
+});
+
 
 app.listen(PORT, () => {
     console.log(`listening on port ${PORT}`);
